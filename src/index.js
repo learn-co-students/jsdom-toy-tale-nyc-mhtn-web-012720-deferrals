882b15bf845a9ff544362fe73058759ebf528284
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-04-17 14:37:04 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,39 +3,35 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
-  const toyDiv = document.querySelector('#toy-collection')
-  const newToyForm = document.querySelector('.add-toy-form')
+  const toyCollectionDiv = document.querySelector('#toy-collection')
+  const addToyForm = document.querySelector('.add-toy-form')
 
+  //fetch toys from database
   fetch('http://localhost:3000/toys')
-  .then(function(response){
-    return response.json()
-  })
-  .then(function(toysArray){
-    renderToys(toysArray)
-  })
+  .then(response => response.json())
+  .then(toyArray => toyArray.forEach(toy => displayToys(toy)))
 
-  const renderToys = (toysArray) => {
-    toysArray.forEach(toy => {
-      toyDiv.innerHTML += `
-      <div class="card">
-        <h2>${toy.name}</h2>
-        <img src=${toy.image} class="toy-avatar" />
-        <p>${toy.likes} Likes </p>
-        <button data-id=${toy.id} class="like-btn">Like <3</button>
-        <button data-id=${toy.id} class="delete-btn">Delete</button>
-      </div>
-      `
-    })
+  //create HTML to display toys on the DOM
+  const displayToys = (toy) => {
+    toyCollectionDiv.innerHTML += `
+    <div class="card">
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar" />
+      <p>${toy.likes} Likes </p>
+      <button data-id=${toy.id} class="like-btn">Like <3</button>
+      <button data-id=${toy.id} class="delete-btn">Delete</button>
+    </div>
+    `
   };
 
-  newToyForm.addEventListener('submit', function(event){
+  //capture user input and update database
+  addToyForm.addEventListener('submit', function(event){
     event.preventDefault()
     let toyName = event.target.name.value
     let toyImage = event.target.image.value
-    //* have input values */
-    //* need to slap them on the dom */
+
     fetch('http://localhost:3000/toys', {
-      method: 'POST',
+      method: "POST",
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
@@ -43,51 +39,53 @@ document.addEventListener("DOMContentLoaded", () => {
       body: JSON.stringify({
         name: toyName,
         image: toyImage,
-        likes: 0
-      }) 
+        likes: 0 
+      })
     })
-    .then(function(response){
-      return response.json()
-    })
-    .then(function(newToy){
-      let newToyHTML = `
-      <div class="card">
-        <h2>${newToy.name}</h2>
-        <img src=${newToy.image} class="toy-avatar" />
-        <p>${newToy.likes} Likes </p>
-        <button data-id=${newToy.id} class="like-btn">Like <3</button>
-        <button data-id=${newToy.id} class="delete-btn">Delete</button>
-      </div>
-      `
-      toyDiv.innerHTML += newToyHTML
-    })
+    .then(response => response.json())
+    .then(newToy => createNewToy(newToy))
   });
 
-  toyDiv.addEventListener('click', function(event){
+  //create new toy html and slap it on the DOM
+  const createNewToy = (newToy) => {
+    toyCollectionDiv.innerHTML += `
+    <div class="card">
+      <h2>${newToy.name}</h2>
+      <img src=${newToy.image} class="toy-avatar" />
+      <p>${newToy.likes} Likes </p>
+      <button data-id=${newToy.id} class="like-btn">Like <3</button>
+      <button data-id=${newToy.id} class="delete-btn">Delete</button>
+    </div>
+    `
+    addToyForm.reset()
+  };
+
+  //add functionality to likes button and delete button
+  toyCollectionDiv.addEventListener('click', function(event){
     if(event.target.className === 'like-btn'){
-      let likes = parseInt(event.target.previousElementSibling.innerHTML)
+      let likes = parseInt(event.target.previousElementSibling.innerText)
       let newLikes = likes + 1
-      event.target.previousElementSibling.innerHTML = newLikes + ' Likes'
-  
-      fetch(`http://localhost:3000/toys/${event.target.dataset.id}`, {
-        method: 'PATCH',
+      event.target.previousElementSibling.innerText = newLikes + " Likes"
+      fetch(`http://localhost:3000/toys/${event.target.dataset.id}`,{
+        method: "PATCH",
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json'
         },
-        body: JSON.stringify({
+        body: JSON.stringify ({
           likes: newLikes
         })
       })
     }
-    if (event.target.className === 'delete-btn'){
-      fetch(`http://localhost:3000/toys/${event.target.dataset.id}`,{
-        method: 'DELETE'
+    if(event.target.className === 'delete-btn'){
+      fetch(`http://localhost:3000/toys/${event.target.dataset.id}`, {
+        method: "DELETE"
       })
-      event.target.parentElement.remove() 
+      event.target.parentElement.remove()
     }
-  });
+  })
 
+  
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
